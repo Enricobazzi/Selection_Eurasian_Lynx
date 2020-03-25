12cccddb04eb9660554c1f874850f3915654c352
Enrico Bazzicalupo <Y6006669A@MacBook-Pro-de-Enrico.local>
2020-03-25 12:01:32 +0100

    parallel calling of all scaffolds launched

--- Variables_WorldClim.R
+++ Variables_WorldClim.R
@@ -53,3 +53,16 @@ for (i in 1:length(populations)){
 }
 
 write.table(x = table,file = "WorldClim_biovariables.txt",quote=FALSE, col.names = T, row.names = FALSE, sep= "\t")
+
+
+## POLYGONS
+
+# with all Worldclim variables for YAKUTIA population ("ya") it would be
+bios <- worldclim[[c("bio1","bio2","bio3","bio4","bio5","bio6","bio7","bio8","bio9","bio10","bio11","bio12","bio13","bio14","bio15","bio16","bio17","bio18","bio19")]]
+names(bios) <- c("bio1","bio2","bio3","bio4","bio5","bio6","bio7","bio8","bio9","bio10","bio11","bio12","bio13","bio14","bio15","bio16","bio17","bio18","bio19")
+coord_table <- read_delim("~/Dropbox/LL_LC_LR_Databases/CSV_LL_selection_coordinates_noNA.csv", col_names = T, delim = ',')
+coord_pop <- coord_table %>% filter(coord_table$pop == "ya")
+coords <- data.frame(x=coord_pop$longitude,y=coord_pop$latitude)
+points <- SpatialPoints(coords, proj4string = bios@crs)
+values <- extract(bios,points)
+df <- cbind.data.frame(coordinates(points),values)
